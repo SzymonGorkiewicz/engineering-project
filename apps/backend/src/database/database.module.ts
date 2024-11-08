f18dc85b7e352994b15dcf45459b329fd4259c15
repoSZ <s.gorkiewicz@interia.pl>
2024-11-08 22:34:28 +0100

    deploying backend and frontend v3

--- apps/backend/src/database/database.module.ts
+++ apps/backend/src/database/database.module.ts
@@ -7,16 +7,31 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: String(configService.get('POSTGRES_PASSWORD')),
-        database: configService.get('POSTGRES_DB'),
-        entities: [__dirname + '/../**/**/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
+      useFactory: (configService: ConfigService) => {
+        // type: 'postgres',
+        // host: configService.get('POSTGRES_HOST'),
+        // port: configService.get('POSTGRES_PORT'),
+        // username: configService.get('POSTGRES_USER'),
+        // password: String(configService.get('POSTGRES_PASSWORD')),
+        // database: configService.get('POSTGRES_DB'),
+        // entities: [__dirname + '/../**/**/**/*.entity{.ts,.js}'],
+        // synchronize: true,
+        const isProduction = configService.get('HEROKU_DATABASE_URL') !== undefined;
+
+        return {
+          type: 'postgres',
+          url: isProduction ? configService.get<string>('HEROKU_DATABASE_URL') : undefined,
+          host: isProduction ? undefined : configService.get('POSTGRES_HOST'),
+          port: isProduction ? undefined : parseInt(configService.get<string>('POSTGRES_PORT'), 10),
+          username: isProduction ? undefined : configService.get('POSTGRES_USER'),
+          password: isProduction ? undefined : configService.get<string>('POSTGRES_PASSWORD'),
+          database: isProduction ? undefined : configService.get('POSTGRES_DB'),
+          ssl: isProduction ? { rejectUnauthorized: false } : undefined,  // Wymuszamy SSL tylko na produkcji
+          entities: [__dirname + '/../**/**/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        };
+
+      },
       inject: [ConfigService],
     }),
   ],
