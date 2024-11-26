ba41f0e142994978f003918587aae94e6a60c0f5
repoSZ <s.gorkiewicz@interia.pl>
2024-11-26 16:12:36 +0100

    Eslint error fixed v2

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin',
-            'security',
-            'no-secrets',
-            'xss',
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'security',
+    'no-secrets',
+    'xss',
   ],
   extends: [
     'plugin:@typescript-eslint/recommended',
@@ -21,27 +22,28 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js','app.e2e-spec.ts'],
+  ignorePatterns: ['.eslintrc.js', 'app.e2e-spec.ts'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'error',
-    "@typescript-eslint/no-misused-promises": "error",
-    "@typescript-eslint/no-var-requires": "error",
-    "@typescript-eslint/ban-ts-comment": "error",
-    "@typescript-eslint/restrict-plus-operands": "error",
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/restrict-plus-operands': 'error',
     'no-secrets/no-secrets': 'error',
     'xss/no-mixed-html': 'warn',
     'xss/no-location-href-assign': 'error',
-    "security/detect-buffer-noassert": "error",  // Wykrywa niebezpieczne użycie funkcji Buffer.
-    "security/detect-child-process": "error",    // Ostrzega przed używaniem child_process, co może być niebezpieczne.
-    "security/detect-eval-with-expression": "error", // Wykrywa potencjalnie niebezpieczne użycie eval.
-    "security/detect-new-buffer": "error",       // Wykrywa stare i niebezpieczne sposoby tworzenia Buffer.
-    "security/detect-no-csrf-before-method-override": "warn",  // Pomaga w identyfikacji potencjalnych problemów z CSRF.
-    "security/detect-non-literal-fs-filename": "warn",  // Wykrywa niebezpieczne operacje na plikach.
-    "security/detect-non-literal-regexp": "warn", // Ostrzega przed dynamicznie tworzonymi wyrażeniami regularnymi.
-    "security/detect-non-literal-require": "error",  // Zapobiega dynamicznemu importowi modułów.
-    "security/detect-possible-timing-attacks": "warn",  // Wykrywa możliwe ataki timingowe.
-    "security/detect-pseudoRandomBytes": "warn"  // Ostrzega przed używaniem pseudolosowych generatorów liczb.
+    'security/detect-buffer-noassert': 'error', // Wykrywa niebezpieczne użycie funkcji Buffer.
+    'security/detect-child-process': 'error', // Ostrzega przed używaniem child_process, co może być niebezpieczne.
+    'security/detect-eval-with-expression': 'error', // Wykrywa potencjalnie niebezpieczne użycie eval.
+    'security/detect-new-buffer': 'error', // Wykrywa stare i niebezpieczne sposoby tworzenia Buffer.
+    'security/detect-no-csrf-before-method-override': 'warn', // Pomaga w identyfikacji potencjalnych problemów z CSRF.
+    'security/detect-non-literal-fs-filename': 'warn', // Wykrywa niebezpieczne operacje na plikach.
+    'security/detect-non-literal-regexp': 'warn', // Ostrzega przed dynamicznie tworzonymi wyrażeniami regularnymi.
+    'security/detect-non-literal-require': 'error', // Zapobiega dynamicznemu importowi modułów.
+    'security/detect-possible-timing-attacks': 'warn', // Wykrywa możliwe ataki timingowe.
+    'security/detect-pseudoRandomBytes': 'warn', // Ostrzega przed używaniem pseudolosowych generatorów liczb.
   },
 };
